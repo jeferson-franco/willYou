65299d83e6db45af12526f0ee669c6b1165efd1a
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-20 14:35:35 -0300

    feat: create willYou.js

---
+++ src/willYou.js
@@ -0,0 +1,12 @@
+function solution(young, beautiful, loved) {
+    return (young && beautiful) !== loved;
+}
+
+module.exports = solution;
+
+// npm init -y
+// npm i jest-cli -g
+// package.json: "scripts": { "test": "jest" }
+// npm test willYou
+
+// alternative solution
